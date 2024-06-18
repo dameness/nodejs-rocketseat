b37f2aef7ede26044f9f9ca6a5d3f75281e8808f
bernardo damiani <bmdamiani17@gmail.com>
2024-06-18 16:01:58 -0300

    feat: incomplete post route

--- daily-diet/src/routes/meals.ts
+++ daily-diet/src/routes/meals.ts
@@ -1,7 +1,32 @@
 import { FastifyInstance } from 'fastify';
+import { z } from 'zod';
+import { checkSessionIdExists } from '../middleware/check-session-id-exists';
+import { knex } from '../database';
+import { randomUUID } from 'node:crypto';
 
 export async function MealsRoutes(app: FastifyInstance) {
-  app.post('/', async (req, res) => {});
+  app.post('/', { preHandler: [checkSessionIdExists] }, async (req, res) => {
+    const createMealBodySchema = z.object({
+      name: z.string(),
+      description: z.optional(z.string()),
+      isOnDiet: z.boolean(),
+      time: z.coerce.date(),
+    });
+
+    const { name, description, isOnDiet, time } = createMealBodySchema.parse(
+      req.body
+    );
+
+    await knex('meals').insert({
+      id: randomUUID(),
+      name,
+      description,
+      isOnDiet,
+      time, //ARRUMAR
+    });
+
+    return res.status(201).send();
+  });
   app.get('/', async (req, res) => {});
   app.get('/:id', async (req, res) => {});
   app.get('/summary/:userId', async (req, res) => {});
