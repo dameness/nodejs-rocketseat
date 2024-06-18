ee78e7ae1ea887e0083be63ffcff9d4bf16d9161
bernardo damiani <bmdamiani17@gmail.com>
2024-06-18 14:30:30 -0300

    feat: add routes structure

--- daily-diet/src/app.ts
+++ daily-diet/src/app.ts
@@ -1,6 +1,8 @@
 import fastify from 'fastify';
 import { MealsRoutes } from './routes/meals';
+import { UsersRoutes } from './routes/users';
 
 export const app = fastify();
 
+app.register(UsersRoutes, { prefix: 'users' });
 app.register(MealsRoutes, { prefix: 'meals' });
--- daily-diet/src/routes/meals.ts
+++ daily-diet/src/routes/meals.ts
@@ -1,7 +1,10 @@
 import { FastifyInstance } from 'fastify';
 
 export async function MealsRoutes(app: FastifyInstance) {
-  app.get('/', async (req, res) => {
-    res.send('hello');
-  });
+  app.post('/', async (req, res) => {});
+  app.get('/', async (req, res) => {});
+  app.get('/:id', async (req, res) => {});
+  app.get('/summary/:userId', async (req, res) => {});
+  app.put('/:id', async (req, res) => {});
+  app.delete('/:id', async (req, res) => {});
 }
