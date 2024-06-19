0468d7766b1a4ad913bcd749f7a591b2a328a828
bernardo damiani <bmdamiani17@gmail.com>
2024-06-19 14:35:54 -0300

    feat: add meals routes

--- daily-diet/src/routes/meals.ts
+++ daily-diet/src/routes/meals.ts
@@ -22,14 +22,87 @@ export async function MealsRoutes(app: FastifyInstance) {
       name,
       description,
       isOnDiet,
-      time, //ARRUMAR
+      time: time.getTime().toString(),
+      user_id: req.user?.id,
     });
 
     return res.status(201).send();
   });
-  app.get('/', async (req, res) => {});
-  app.get('/:id', async (req, res) => {});
-  app.get('/summary/:userId', async (req, res) => {});
-  app.put('/:id', async (req, res) => {});
-  app.delete('/:id', async (req, res) => {});
+  app.get('/', { preHandler: [checkSessionIdExists] }, async (req, res) => {
+    const meals = await knex('meals').where({ user_id: req.user?.id });
+    return { meals };
+  });
+  app.get('/:id', { preHandler: [checkSessionIdExists] }, async (req, res) => {
+    const getMealByIdSchema = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = getMealByIdSchema.parse(req.params);
+
+    const meal = await knex('meals')
+      .where({ id, user_id: req.user?.id })
+      .first();
+
+    if (!meal) {
+      return res.status(404).send({ error: 'Not Found.' });
+    }
+
+    return { meal };
+  });
+  app.get(
+    '/summary/:userId',
+    { preHandler: [checkSessionIdExists] },
+    async (req, res) => {}
+  );
+  app.put('/:id', { preHandler: [checkSessionIdExists] }, async (req, res) => {
+    const editMealBodySchema = z.object({
+      name: z.optional(z.string()),
+      description: z.optional(z.string()),
+      isOnDiet: z.optional(z.boolean()),
+      time: z.optional(z.coerce.date()),
+    });
+
+    const { name, description, isOnDiet, time } = editMealBodySchema.parse(
+      req.body
+    );
+    const getMealByIdSchema = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = getMealByIdSchema.parse(req.params);
+
+    const meal = await knex('meals')
+      .where({ id, user_id: req.user?.id })
+      .first();
+
+    if (!meal) {
+      return res.status(404).send({ error: 'Not Found.' });
+    }
+
+    await knex('meals')
+      .update({ name, description, isOnDiet, time: time?.getTime().toString() })
+      .where({ id, user_id: req.user?.id });
+
+    return res.status(204).send();
+  });
+  app.delete(
+    '/:id',
+    { preHandler: [checkSessionIdExists] },
+    async (req, res) => {
+      const getMealByIdSchema = z.object({
+        id: z.string().uuid(),
+      });
+
+      const { id } = getMealByIdSchema.parse(req.params);
+
+      const meal = await knex('meals')
+        .where({ id, user_id: req.user?.id })
+        .first();
+
+      if (!meal) {
+        return res.status(404).send({ error: 'Not Found.' });
+      }
+      // FINALIZAR
+    }
+  );
 }
