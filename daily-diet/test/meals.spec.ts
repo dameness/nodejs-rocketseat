7237404fa75ed8923bbd6d86e40734d567520993
bernardo damiani <bmdamiani17@gmail.com>
2024-06-20 14:59:10 -0300

    test: add test to get http requests

--- daily-diet/test/meals.spec.ts
+++ daily-diet/test/meals.spec.ts
@@ -35,9 +35,126 @@ describe('meals routes', () => {
       })
       .expect(201);
   });
-  // it('should be able get all meals of a user', async () => {});
-  // it('should be able to get one meal', async () => {});
-  // it('should be able to get a user diet summary', async () => {});
+  it('should be able get all meals of a user', async () => {
+    const userResponse = await request(app.server)
+      .post('/users')
+      .send({ name: 'name', email: 'mail@mail.com' })
+      .expect(201);
+
+    const cookies = userResponse.get('Set-Cookie');
+
+    await request(app.server)
+      .post('/meals')
+      .set('Cookie', cookies!)
+      .send({
+        name: 'test-meal',
+        isOnDiet: true,
+        time: '2022/02/01',
+      })
+      .expect(201);
+
+    await request(app.server)
+      .post('/meals')
+      .set('Cookie', cookies!)
+      .send({
+        name: 'test-meal-2',
+        isOnDiet: true,
+        time: '2022/02/01',
+      })
+      .expect(201);
+
+    const mealsResponse = await request(app.server)
+      .get('/meals')
+      .set('Cookie', cookies!)
+      .expect(200);
+
+    expect(mealsResponse.body.meals).toHaveLength(2);
+
+    expect(mealsResponse.body.meals[0]).toEqual(
+      expect.objectContaining({ name: 'test-meal' })
+    );
+
+    expect(mealsResponse.body.meals[1]).toEqual(
+      expect.objectContaining({ name: 'test-meal-2' })
+    );
+  });
+  it('should be able to get one meal', async () => {
+    const userResponse = await request(app.server)
+      .post('/users')
+      .send({ name: 'name', email: 'mail@mail.com' })
+      .expect(201);
+
+    const cookies = userResponse.get('Set-Cookie');
+
+    await request(app.server)
+      .post('/meals')
+      .set('Cookie', cookies!)
+      .send({
+        name: 'test-meal',
+        isOnDiet: true,
+        time: '2022/02/01',
+      })
+      .expect(201);
+
+    const mealsResponse = await request(app.server)
+      .get('/meals')
+      .set('Cookie', cookies!)
+      .expect(200);
+
+    const { id } = mealsResponse.body.meals[0];
+
+    const singleMealResponse = await request(app.server)
+      .get(`/meals/${id}`)
+      .set('Cookie', cookies!)
+      .expect(200);
+
+    expect(singleMealResponse.body.meal).toEqual(
+      expect.objectContaining({
+        id,
+        name: 'test-meal',
+      })
+    );
+  });
+  it('should be able to get a user diet summary', async () => {
+    const userResponse = await request(app.server)
+      .post('/users')
+      .send({ name: 'name', email: 'mail@mail.com' })
+      .expect(201);
+
+    const cookies = userResponse.get('Set-Cookie');
+
+    await request(app.server)
+      .post('/meals')
+      .set('Cookie', cookies!)
+      .send({
+        name: 'test-meal',
+        isOnDiet: false,
+        time: '2022/02/01',
+      })
+      .expect(201);
+
+    await request(app.server)
+      .post('/meals')
+      .set('Cookie', cookies!)
+      .send({
+        name: 'test-meal-2',
+        isOnDiet: true,
+        time: '2022/02/01',
+      })
+      .expect(201);
+
+    const summaryResponse = await request(app.server)
+      .get('/meals/summary')
+      .set('Cookie', cookies!)
+      .expect(200);
+
+    expect(summaryResponse.body).toEqual({
+      total: 2,
+      onDiet: 1,
+      offDiet: 1,
+      bestStreakOnDiet: 1,
+    });
+  });
   // it('should be able to edit a meal', async () => {});
   // it('should be able to delete a meal', async () => {});
 });
