5de3875c14224dfd2490ba1230f49f951b800e9b
bernardo damiani <bmdamiani17@gmail.com>
2024-06-25 13:57:52 -0300

    feat: initial routes config

---
+++ api-solid/src/http/controllers/register.ts
@@ -0,0 +1,32 @@
+import { z } from 'zod';
+import { prisma } from '@/lib/prisma';
+import { hash } from 'bcryptjs';
+import { FastifyReply, FastifyRequest } from 'fastify';
+
+export const register = async (req: FastifyRequest, res: FastifyReply) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  });
+
+  const { name, email, password } = registerBodySchema.parse(req.body);
+
+  const password_hash = await hash(password, 6);
+
+  const userWithSameEmail = await prisma.user.findUnique({ where: { email } });
+
+  if (userWithSameEmail) {
+    return res.status(409).send();
+  }
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash,
+    },
+  });
+
+  return res.status(201).send();
+};
