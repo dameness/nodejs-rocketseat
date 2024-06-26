4d9b89ec010c5a6be32983bfbe19d6dd468189c0
bernardo damiani <bmdamiani17@gmail.com>
2024-06-26 15:59:16 -0300

    feat: starting to use repository pattern

--- api-solid/src/http/controllers/register.ts
+++ api-solid/src/http/controllers/register.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
-import { prisma } from '@/lib/prisma';
-import { hash } from 'bcryptjs';
+
 import { FastifyReply, FastifyRequest } from 'fastify';
+import { registerUseCase } from '@/services/users/register';
 
 export const register = async (req: FastifyRequest, res: FastifyReply) => {
   const registerBodySchema = z.object({
@@ -12,21 +12,11 @@ export const register = async (req: FastifyRequest, res: FastifyReply) => {
 
   const { name, email, password } = registerBodySchema.parse(req.body);
 
-  const password_hash = await hash(password, 6);
-
-  const userWithSameEmail = await prisma.user.findUnique({ where: { email } });
-
-  if (userWithSameEmail) {
+  try {
+    await registerUseCase({ name, email, password });
+  } catch (err) {
     return res.status(409).send();
   }
-
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash,
-    },
-  });
 
   return res.status(201).send();
 };
---
+++ api-solid/src/repositories/prisma-users-repository.ts
@@ -0,0 +1,9 @@
+import { prisma } from '@/lib/prisma';
+import { Prisma } from '@prisma/client';
+export class PrismaUsersRepository {
+  async create(data: Prisma.UserCreateInput) {
+    await prisma.user.create({
+      data,
+    });
+  }
+}
