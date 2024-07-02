7b1c8be6a754884e19db7cb66156c5e86ec01e93
bernardo damiani <bmdamiani17@gmail.com>
2024-07-02 18:50:47 -0300

    feat: add password hash test when registering an user

--- api-solid/src/services/users/register.spec.ts
+++ api-solid/src/services/users/register.spec.ts
@@ -1,5 +1,37 @@
-import { expect, it } from 'vitest';
+import { describe, expect, it } from 'vitest';
+import { RegisterUseCase } from './register';
+import { compare } from 'bcryptjs';
 
-it('should work', () => {
-  expect(2 + 2).toBe(4);
+describe('Register Use Case', () => {
+  it('should hash user password upon registration', async () => {
+    //unit test!! - não acessa o banco de dado, testa a pequena parte, rápido
+    const registerUseCase = new RegisterUseCase({
+      async create(data) {
+        return {
+          id: 'user-1',
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+        };
+      },
+
+      async findByEmail(email) {
+        return null;
+      },
+    });
+
+    const { user } = await registerUseCase.execute({
+      name: 'test',
+      email: 'test@mail.com',
+      password: '123423',
+    });
+
+    const isPasswordCorrectlyHashed = await compare(
+      '123423',
+      user.password_hash
+    );
+
+    expect(isPasswordCorrectlyHashed).toBe(true);
+  });
 });
