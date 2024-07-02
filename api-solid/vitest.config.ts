e6f11b2e39409ab58567c9e7998676f4e509f958
bernardo damiani <bmdamiani17@gmail.com>
2024-07-02 17:28:20 -0300

    feat: initial test setup

---
+++ api-solid/vitest.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vitest/config';
+import tsconfigPaths from 'vite-tsconfig-paths';
+export default defineConfig({
+  test: {
+    globals: true,
+    reporters: 'verbose',
+
+    // This fix the conflict between tests switches and the single database
+    pool: 'forks',
+    poolOptions: {
+      forks: {
+        singleFork: true,
+      },
+    },
+  },
+  plugins: [tsconfigPaths()],
+});
