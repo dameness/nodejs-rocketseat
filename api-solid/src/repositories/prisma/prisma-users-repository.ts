23df31223c6134034929cb812edcb28bb0b7c524
bernardo damiani <bmdamiani17@gmail.com>
2024-07-02 18:50:15 -0300

    feat: change users repository return

--- api-solid/src/repositories/prisma/prisma-users-repository.ts
+++ api-solid/src/repositories/prisma/prisma-users-repository.ts
@@ -3,9 +3,10 @@ import { UsersRepository } from '@/models/users-repository';
 import { Prisma } from '@prisma/client';
 export class PrismaUsersRepository implements UsersRepository {
   async create(data: Prisma.UserCreateInput) {
-    await prisma.user.create({
+    const user = await prisma.user.create({
       data,
     });
+    return user;
   }
 
   async findByEmail(email: string) {
--- api-solid/src/services/users/register.ts
+++ api-solid/src/services/users/register.ts
@@ -2,6 +2,7 @@ import { prisma } from '@/lib/prisma';
 import { UsersRepository } from '@/models/users-repository';
 import { hash } from 'bcryptjs';
 import { UserAlreadyExistsError } from '../errors/user-already-exists-error';
+import { User } from '@prisma/client';
 
 interface RegisterUseCaseRequest {
   name: string;
@@ -9,10 +10,18 @@ interface RegisterUseCaseRequest {
   password: string;
 }
 
+interface RegisterUseCaseResponse {
+  user: User;
+}
+
 export class RegisterUseCase {
   constructor(private usersRepository: UsersRepository) {} // ao colocar "private", usersRepository será também declarado aqui
 
-  async execute({ name, email, password }: RegisterUseCaseRequest) {
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
     {
       const password_hash = await hash(password, 6);
 
@@ -22,11 +31,13 @@ export class RegisterUseCase {
         throw new UserAlreadyExistsError();
       }
 
-      await this.usersRepository.create({
+      const user = await this.usersRepository.create({
         name,
         email,
         password_hash,
       });
+
+      return { user };
     }
   }
 }
