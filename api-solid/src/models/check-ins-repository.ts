67ff7bace50833e282f7813c4cf9077d584ead5a
bernardo damiani <bmdamiani17@gmail.com>
2024-07-31 18:24:02 -0300

    feat: fetch user checkin history(initial implementation)

--- api-solid/src/models/check-ins-repository.ts
+++ api-solid/src/models/check-ins-repository.ts
@@ -4,4 +4,11 @@ export interface CheckInsRepository {
   // UncheckedCreateInput --> ver anotação no use case
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>;
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>;
+  findManyByUserId({
+    userId,
+    page,
+  }: {
+    userId: string;
+    page: number;
+  }): Promise<CheckIn[]>;
 }
