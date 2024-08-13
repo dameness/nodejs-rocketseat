20d2c5b48824fe9f265cad6ccaa5e77ed486d71b
bernardo damiani <bmdamiani17@gmail.com>
2024-08-13 13:51:29 -0300

    feat: create new database schemas for e2e tests

--- api-solid/prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ api-solid/prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,11 +1,44 @@
+import 'dotenv/config'; // le o arquivo env e interpreta as variáveis como variáveis ambiente
+
+import { randomUUID } from 'node:crypto';
+import { execSync } from 'node:child_process';
 import { Environment } from 'vitest';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable.');
+  }
+
+  const url = new URL(process.env.DATABASE_URL);
+
+  url.searchParams.set('schema', schema);
+
+  return url.toString();
+}
 
 export default <Environment>{
   name: 'prisma',
   async setup() {
-    console.log('Executou');
+    const schema = randomUUID();
+
+    const databaseURL = generateDatabaseURL(schema);
+
+    process.env.DATABASE_URL = databaseURL;
+
+    execSync('npx prisma migrate deploy'); // usando migrate deploy ao invés de migrate dev pois ele
+    // pula a etapa de verificação do schema com o local e só
+    // executa os comandos
     return {
-      async teardown() {},
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`
+        );
+
+        await prisma.$disconnect();
+      },
     };
   },
   transformMode: 'ssr',
