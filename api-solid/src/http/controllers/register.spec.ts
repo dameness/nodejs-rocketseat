617a6c7581997025c1b5a030e7c2d4ee7e399ae3
bernardo damiani <bmdamiani17@gmail.com>
2024-08-13 14:10:57 -0300

    feat: add register user e2e test

--- api-solid/src/http/controllers/register.spec.ts
+++ api-solid/src/http/controllers/register.spec.ts
@@ -1,3 +1,23 @@
-import { it } from 'vitest';
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
+import { app } from '@/app';
+import request from 'supertest';
 
-it('ok', () => {});
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should be able to register a user', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'nome',
+      email: 'mail@mail.com',
+      password: '123456',
+    });
+
+    expect(response.statusCode).toEqual(201);
+  });
+});
